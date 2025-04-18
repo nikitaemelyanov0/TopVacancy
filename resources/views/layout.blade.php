813043348f214241c8db87ef1c82fae2d5db4701
nikitaemelyanov0 <156890965+nikitaemelyanov0@users.noreply.github.com>
2025-04-19 00:38:46 +0500

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/css/app.css')}}">
     <link rel="icon" href="favicon.ico">
 </head>
 <body>
