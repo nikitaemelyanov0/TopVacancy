37fd8194abcc9279e166179a90eb17858bebb9d9
nikitaemelyanov0 <156890965+nikitaemelyanov0@users.noreply.github.com>
2025-04-19 01:36:38 +0500

    Update VacancyController.php

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -7,6 +7,7 @@ use App\Models\Vacancy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class VacancyController extends Controller
 {
