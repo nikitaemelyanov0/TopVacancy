b0fe262cddc1a05a2c0a84a90c6f0a3c68f86028
nikitaemelyanov0 <156890965+nikitaemelyanov0@users.noreply.github.com>
2025-06-09 23:54:59 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,7 @@ Route::get('/', [VacancyController::class, 'vacanciesAtHome'])->name('home');
 
 Route::get('/resume/{id}', [ResumeController::class, 'resumeIndex'])->name('resume.index');
 
-Route::delete('resume/{id}/delete', [ResumeController::class, 'destroy'])->name('resume.destroy');
-
-Route::get('resume/{id}/edit', [ResumeController::class, 'edit'])->name('resume.edit');
-Route::put('resume/{id}/edit', [ResumeController::class, 'update'])->name('resume.update');
-
 Route::get('/vacancy/{id}', [VacancyController::class, 'vacancyIndex'])->name('vacancy.index');
-
-Route::delete('vacancy/{id}/delete', [VacancyController::class, 'destroy'])->name('vacancy.destroy');
-
-Route::get('vacancy/{id}/edit', [VacancyController::class, 'edit'])->name('vacancy.edit');
-Route::put('vacancy/{id}/edit', [VacancyController::class, 'update'])->name('vacancy.update');
 
 Route::get('/registration', [UserController::class, 'registrationIndex'])->name('registration');
 Route::post('/registration', [UserController::class, 'createUser']);
@@ -29,22 +19,32 @@ Route::post('/registration', [UserController::class, 'createUser']);
 Route::get('/authorization', [UserController::class, 'authorizationIndex'])->name('login');
 Route::post('/authorization', [UserController::class, 'loginPost']);
 
-Route::get('delete_user', [UserController::class, 'delete_user']);
-Route::post('delete_user', [UserController::class, 'delete_user'])->name('delete_user');
-
 Route::get('/search_vacancy',[VacancyController::class, 'searchVacancy'])->name('search_vacancy');
 Route::get('/search_vacancy/{company}',[VacancyController::class, 'searchCompany'])->name('search_company');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/create_resume', [ResumeController::class, 'createResumeIndex'])->name('create_resume.index');
     Route::post('/create_resume', [ResumeController::class, 'createResume']);
-
+    
+    Route::get('resume/{id}/edit', [ResumeController::class, 'edit'])->name('resume.edit');
+    Route::put('resume/{id}/edit', [ResumeController::class, 'update'])->name('resume.update');
+    
+    Route::delete('resume/{id}/delete', [ResumeController::class, 'destroy'])->name('resume.destroy');
+    
     Route::get('/create_vacancy', [VacancyController::class, 'createVacancyIndex'])->name('create_vacancy.index');
     Route::post('/create_vacancy', [VacancyController::class, 'createVacancy']);
+    
+    Route::get('vacancy/{id}/edit', [VacancyController::class, 'edit'])->name('vacancy.edit');
+    Route::put('vacancy/{id}/edit', [VacancyController::class, 'update'])->name('vacancy.update');
+
+    Route::delete('vacancy/{id}/delete', [VacancyController::class, 'destroy'])->name('vacancy.destroy');
 
     Route::get('/application',[ApplicationController::class, 'applicationIndex'])->name('application.index');
     Route::post('/application/{id}',[ApplicationController::class, 'makeApplication'])->name('application.store');
 
+    Route::get('delete_user', [UserController::class, 'delete_user']);
+    Route::post('delete_user', [UserController::class, 'delete_user'])->name('delete_user');
+    
     Route::get('/admin', [AdminController::class, 'adminIndex'])->name('admin');
 });
 
